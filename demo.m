1e63cc5cdbfdae197f48b9d83fdba9065257fe6c
anguie288 <61396665+anguie288@users.noreply.github.com>
2020-03-14 19:43:30 -0400

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,15 @@
+//
+//********SweetTutos***************
+//****www.sweettutos.com*******
+//
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    NSLog(@"log hello");//This will output Hello World!
+
+    [pool release];
+    return 0;
+}
